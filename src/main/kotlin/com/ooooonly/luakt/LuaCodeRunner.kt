1e56abad9050144c6aa6f6b03c2cd712b15d0322
only52607 <1361822439@qq.com>
2020-08-25 23:10:20 +0800

    add run file

--- src/main/kotlin/com/ooooonly/luakt/LuaCodeRunner.kt
+++ src/main/kotlin/com/ooooonly/luakt/LuaCodeRunner.kt
@@ -6,7 +6,10 @@ import org.luaj.vm2.lib.jse.JsePlatform
 val standardGlobals: Globals by lazy {
     JsePlatform.standardGlobals()
 }
-
 fun runLua(code: String, globals: Globals = standardGlobals) {
     globals.load(code).call()
+}
+
+fun runLuaFile(filePath: String, globals: Globals = standardGlobals) {
+    globals.loadfile(filePath).call()
 }
