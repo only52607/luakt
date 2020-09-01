47247390afe00566a61d3da37f7048cf73afdc84
only52607 <1361822439@qq.com>
2020-09-01 15:39:24 +0800

    Add getOrNull

--- src/main/kotlin/com/ooooonly/luakt/LuaTableEx.kt
+++ src/main/kotlin/com/ooooonly/luakt/LuaTableEx.kt
@@ -5,6 +5,7 @@ import org.luaj.vm2.LuaValue
 
 operator fun LuaTable.get(key: Any): LuaValue = get(key.asLuaValue())
 operator fun LuaTable.set(key: Any, value: Any) = set(key.asLuaValue(), value.asLuaValue())
+fun LuaTable.getOrNull(key: Any): LuaValue? = get(key.asLuaValue())?.takeIf { it != LuaValue.NIL }
 
 fun LuaTable.setAll(table: LuaTable): LuaTable = apply {
     val keys: Array<LuaValue> = table.keys()
