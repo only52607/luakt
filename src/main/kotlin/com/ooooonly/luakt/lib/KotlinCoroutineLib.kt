cbc32ab22cc3e6c68a59f9b0d3e519c47f7dc6c4
only52607 <1361822439@qq.com>
2021-02-14 13:55:32 +0800

    Add KotlinCoroutineLib.kt

---
+++ src/main/kotlin/com/ooooonly/luakt/lib/KotlinCoroutineLib.kt
@@ -0,0 +1,30 @@
+package com.ooooonly.luakt.lib
+
+import com.ooooonly.luakt.edit
+import com.ooooonly.luakt.luaFunctionOf
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.luaj.vm2.Globals
+import org.luaj.vm2.LuaClosure
+import org.luaj.vm2.LuaValue
+import org.luaj.vm2.lib.TwoArgFunction
+
+@Suppress("unused")
+class KotlinCoroutineLib(private val coroutineScope: CoroutineScope) : TwoArgFunction() {
+    override fun call(modName: LuaValue?, env: LuaValue?): LuaValue {
+        val globals: Globals? = env?.checkglobals()
+        globals?.edit {
+            "sleep" to luaFunctionOf { time: Long ->
+                runBlocking { delay(time) }
+            }
+            "launch" to luaFunctionOf { cls: LuaClosure ->
+                return@luaFunctionOf coroutineScope.launch {
+                    cls.invoke()
+                }
+            }
+        }
+        return LuaValue.NIL
+    }
+}
