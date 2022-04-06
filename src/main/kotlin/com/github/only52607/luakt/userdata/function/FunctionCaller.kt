65ebf9fe4ad69ccc5a3596f7ef89ef10c93af1d5
OOOOONLY <ooooonly@foxmail.com>
2022-04-06 14:58:58 +0800

    Add LuaPromise

--- src/main/kotlin/com/github/only52607/luakt/userdata/function/FunctionCaller.kt
+++ src/main/kotlin/com/github/only52607/luakt/userdata/function/FunctionCaller.kt
@@ -1,6 +1,13 @@
 package com.github.only52607.luakt.userdata.function
 
+import com.github.only52607.luakt.ValueMapper
+import com.github.only52607.luakt.userdata.objects.LuaPromise
+import com.github.only52607.luakt.utils.asLuaValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.callSuspendBy
@@ -23,6 +30,27 @@ sealed class FunctionCaller {
                 kFunction.callBy(parametersMap)
             }
         }
+    }
+
+    class Promise(private val valueMapper: ValueMapper) : FunctionCaller(), CoroutineScope, ValueMapper by valueMapper {
+        override fun callFunction(kFunction: KFunction<*>, parametersMap: Map<KParameter, Any?>): Any? {
+            return if (kFunction.isSuspend) {
+                return LuaPromise { receiver ->
+                    launch {
+                        try {
+                            receiver.resolve(kFunction.callSuspendBy(parametersMap).asLuaValue())
+                        } catch (e: Exception) {
+                            receiver.reject(e.asLuaValue())
+                        }
+                    }
+                }
+            } else {
+                kFunction.callBy(parametersMap)
+            }
+        }
+
+        override val coroutineContext: CoroutineContext
+            get() = EmptyCoroutineContext
     }
 
     abstract fun callFunction(kFunction: KFunction<*>, parametersMap: Map<KParameter, Any?>): Any?
